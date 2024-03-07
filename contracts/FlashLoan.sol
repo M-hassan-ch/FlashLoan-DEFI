a5dd5fba09182617bb9c9704b19cf7c9613aa204
Hassan <hassanch5209@gmail.com>
2024-03-07 19:39:57 +0500

    completed functions that trade tokens

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -27,28 +27,102 @@ contract FlashLoan {
     uint256 private constant MAX_INT =
         115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
-    
-    function initiateArbitrage(address _busdBorrow, uint amount) external {
+    function initiateArbitrage(address busdAddress, uint amount) external {
         IERC20(CAKE).safeApprove(address(PANCAKE_ROUTER), MAX_INT);
         IERC20(CROX).safeApprove(address(PANCAKE_ROUTER), MAX_INT);
         IERC20(BUSD).safeApprove(address(PANCAKE_ROUTER), MAX_INT);
 
         address pair = IUniswapV2Factory(PANCAKE_FACTORY).getPair(
-            _busdBorrow,
+            busdAddress,
             WBNB
         );
 
-        require(pair != address(0), "Pool not found");
+        require(pair != address(0), "InitiateArbitrage: Pool not found");
 
         address token0 = IUniswapV2Pair(pair).token0();
         address token1 = IUniswapV2Pair(pair).token1();
 
-        uint amount0ToBorrow = _busdBorrow == token0 ? amount : 0;
-        uint amount1ToBorrow = _busdBorrow == token1 ? amount : 0;
- 
-        bytes memory data = abi.encode(_busdBorrow, amount, msg.sender);
-        IUniswapV2Pair(pair).swap(amount0ToBorrow, amount1ToBorrow, address(this), data);
+        uint amount0ToBorrow = busdAddress == token0 ? amount : 0;
+        uint amount1ToBorrow = busdAddress == token1 ? amount : 0;
 
+        bytes memory data = abi.encode(busdAddress, amount, msg.sender);
+        IUniswapV2Pair(pair).swap(
+            amount0ToBorrow,
+            amount1ToBorrow,
+            address(this),
+            data
+        );
     }
-    
+
+    function pancakeCall(
+        address sender,
+        uint amount0,
+        uint amount1,
+        bytes calldata data
+    ) external {
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+        address pair = IUniswapV2Factory(PANCAKE_FACTORY).getPair(
+            token0,
+            token1
+        );
+
+        require(pair == msg.sender, "PancakeCall: Invalid caller");
+        require(sender == address(this), "PancakeCall: Invalid sender");
+
+        (address busdAddress, uint amount, address userAddress) = abi.decode(
+            data,
+            (address, uint, address)
+        );
+
+        uint fee = ((amount * 3) / 997) + 1;
+        uint repayAmount = amount + fee;
+        uint loanAmount = amount0 > 0 ? amount0 : amount1;
+
+        uint trade1Coin = placeTrade(BUSD, CROX, loanAmount);
+        uint trade2Coin = placeTrade(CROX, CAKE, trade1Coin);
+        uint trade3Coin = placeTrade(CAKE, BUSD, trade2Coin);
+
+        bool profit = checkProfit(repayAmount, trade3Coin);
+        require(profit, "PancakeCall: Arbitrage is not profitable");
+
+        IERC20(BUSD).transfer(userAddress, trade3Coin - repayAmount);
+        IERC20(busdAddress).transfer(pair, repayAmount);
+    }
+
+    function placeTrade(
+        address fromToken,
+        address toToken,
+        uint amountToTrade
+    ) private  returns (uint) {
+        address pair = IUniswapV2Factory(PANCAKE_FACTORY).getPair(
+            fromToken,
+            toToken
+        );
+        require(pair == address(0), "PlaceTrade: Pair not fount");
+        address[] memory path = new address[](2);
+        (path[0], path[1]) = (fromToken, toToken);
+
+        uint estimatedResultToken = IUniswapV2Router01(PANCAKE_ROUTER)
+            .getAmountsOut(amountToTrade, path)[1];
+
+        uint amountReceived = IUniswapV2Router01(PANCAKE_ROUTER)
+            .swapExactTokensForTokens(
+                amountToTrade,
+                estimatedResultToken,
+                path,
+                address(this),
+                deadline
+            )[1];
+
+        require(amountReceived > 0, "PlaceTrade: Transactoin aborted");
+        return amountReceived;
+    }
+
+    function checkProfit(
+        uint repayAmount,
+        uint earnedCoin
+    ) private pure returns (bool) {
+        return earnedCoin > repayAmount;
+    }
 }
